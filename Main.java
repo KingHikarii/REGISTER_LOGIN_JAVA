87328247619ca327423d0626331a42671d023f71
Degnan Kopa <103539937+KingHikarii@users.noreply.github.com>
2023-02-07 13:43:59 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String users[] = new String[3];
+        String passw[] = new String[3];
+        String first_cont[] = new String[3];
+        String second_cont[] = new String[3];
+
+        int i = 0;
+
+        while (true)
+        {
+
+           System.out.println("1. register");
+           System.out.println("2. login");
+           System.out.println("3. display");
+           System.out.println("4. delete");
+           System.out.println("5. search");
+           System.out.println("Enter a selection: ");
+
+           String selection;
+           selection = input.nextLine();
+
+           if(selection.equals("register"))
+           {
+               System.out.print("Create a username: ");
+               while(i < users.length)
+               {
+                   users[i] = input.nextLine();
+                   break;
+               }
+               System.out.print("Create a password: ");
+               while(i < passw.length)
+               {
+                   passw[i] = input.nextLine();
+                   i++;
+                   break;
+               }
+
+           }
+
+           else if(selection.equals("login"))
+           {
+               System.out.print("Enter your username: ");
+               String entered_user = input.nextLine();
+               System.out.print("Enter your password: ");
+               String entered_password = input.nextLine();
+               int counter = 0;
+
+
+               for(i = 0; i< users.length; i++)
+               {
+                   if(entered_user.equals(users[i]) && entered_password.equals(passw[i]))
+                   {
+                       System.out.println("Login Found!");
+                       break;
+                   }
+                   else if(counter < users.length) {
+                       first_cont[i] = users[i];
+                       second_cont[i] = passw[i];
+                       counter++;
+                       if(counter == users.length)
+                       {
+                           System.out.println("User was not found!");
+                           break;
+                       }
+
+                   }
+
+               }
+
+          }
+        else if(selection.equals("Display"))
+           {
+               System.out.println("Showing all users and passwords: ");
+               for(int a = 0; a < users.length; a++)
+               {
+                   System.out.println("USER: " + users[a] + " " + "PASSWORD: " + passw[a]);
+
+               }
+           }
+        }
+
+
+
+    }
+}
